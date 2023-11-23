b4fe4a559770314ba6debe1ed0428e268cdc9492
jmj6352 <jmj6352@naver.com>
2023-11-24 08:54:45 +0900

    course link

--- trip-project/src/store/news.js
+++ trip-project/src/store/news.js
@@ -6,7 +6,7 @@ export const useNeswStore = defineStore("attraction", () => {
   /* ====== 뉴스 크롤링 ========== */
   const news = ref([]); //목록 데이터
   const listNews = async () => {
-    const { data } = await axios.get(`/api/news`);
+    // const { data } = await axios.get(`/api/news`);
     console.log(`listNews() 요청 응답: `, data);
     news.value = data;
   };
